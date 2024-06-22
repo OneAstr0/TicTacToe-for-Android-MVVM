79209eedcb0c5182fdc95940bac4b53741fb76b9
Eugene Fedosov <wendomen@yandex.ru>
2024-06-23 02:15:23 +0300

    Update Player.java

--- app/src/main/java/game/tictactoe/Model/Player.java
+++ app/src/main/java/game/tictactoe/Model/Player.java
@@ -2,11 +2,37 @@ package game.tictactoe.Model;
 
 public class Player {
 
-    public String name;
-    public String value;
+    private String name;
+    private Integer quantity;
+    private Integer[] excess; // coordinates of the cell of the field from where the element will be removed
 
-    public Player(String name, String value) {
+    public Player(String name, Integer quantity, Integer[] excess) {
         this.name = name;
-        this.value = value;
+        this.quantity = quantity;
+        this.excess = excess;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setExcess(Integer[] excess) {
+        this.excess = excess;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Integer[] getExcess() {
+        return excess;
     }
 }
